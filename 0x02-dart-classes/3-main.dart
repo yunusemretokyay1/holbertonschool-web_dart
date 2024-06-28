8199f43529551d775c063aa68a183b0e671df1b5
yunusemretokyay1 <yunusemretokyay@gmail.com>
2024-06-28 11:25:56 +0200

    private password

---
+++ 0x02-dart-classes/3-main.dart
@@ -0,0 +1,10 @@
+import '3-private_password.dart';
+
+void main() {
+  final ps = Password(password: "Passwordcode");
+  print(ps.toString());
+  print(ps.isValid());
+  final ps2 = Password(password: "PasswordDecode3");
+  print(ps2.toString());
+  print(ps2.isValid());
+}
---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,36 @@
+class Password {
+    String _password = '';
+
+    Password({ String password = '' }) {
+        this._password = password;
+    }
+
+    bool isValid() {
+        if (_password.length <= 8 || _password.length >= 16)
+            return false;
+
+        int $A = 'A'.codeUnitAt(0);
+        int $Z = 'Z'.codeUnitAt(0);
+        int $a = 'a'.codeUnitAt(0);
+        int $z = 'z'.codeUnitAt(0);
+        int $0 = '0'.codeUnitAt(0);
+        int $9 = '9'.codeUnitAt(0);
+
+        bool containsUpper = false;
+        bool containsLower = false;
+        bool containsDigit = false;
+
+        for (int codeUnit in _password.codeUnits) {
+            if ($A <= codeUnit && codeUnit <= $Z)
+                containsUpper = true;
+            if ($a <= codeUnit && codeUnit <= $z)
+                containsLower = true;
+            if ($0 <= codeUnit && codeUnit <= $9)
+                containsDigit = true;
+        }
+
+        return containsUpper && containsLower && containsDigit;
+    }
+
+    String toString() => 'Your Password is: $_password';
+}
